71a785b2ee440f4ac35957327b32a39ed240d870
Dan <daniel.e.munro@gmail.com>
2023-02-20 23:14:27 -0700

    Start of streamApi tests

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,23 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class HomeController {
 
     @GetMapping
     public String index() {
         return "index";
     }
+
+    @GetMapping("/streamapi")
+    public StreamApiResponse streamapi() {
+        return new StreamApiResponse("Response");
+    }
+
+    record StreamApiResponse(String streamApi) {}
+
 }
 
